f4f37a9ea42b13d1375ac6edf6adb35853ac2b47
Satyam-R4j <satyamraj418@gmail.com>
2025-07-18 02:37:40 +0530

    Implementing Stack in js

---
+++ 06_Stack_js/Script.js
@@ -0,0 +1,69 @@
+class Stack
+{
+
+    constructor()
+    {
+        this.stack = []
+    }
+
+    push(val)
+    {
+        this.stack.push(val)
+    }
+
+    pop()
+    {
+        if (this.isEmpty())
+        {
+            print("Stack is empty!!")
+            return
+        }
+        else
+        {
+
+            return this.stack.pop()
+        }
+    }
+
+    top()
+    {
+
+        if (this.isEmpty())
+        {
+            print("Stack is empty!!")
+            return
+        }
+        else
+        {
+            return this.stack[this.else.size - 1]
+        }
+    }
+
+    isEmpty()
+    {
+        return this.stack.length === 0
+    }
+
+    size()
+    {
+        return this.stack.length
+    }
+
+    print()
+    {
+        console.log(this.stack.join(' '));
+
+    }
+}
+
+let stc = new Stack()
+stc.push(2)
+stc.push(2)
+stc.push(2)
+
+stc.print()
+
+
+
+size = stc.size()
+console.log(size);
