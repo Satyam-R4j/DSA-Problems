4a66ef908a849a1f6b88fef1a8db13ea441bfee8
Satyam-R4j <satyamraj418@gmail.com>
2025-08-14 22:01:38 +0530

    Queeue implementation in js

---
+++ 11_Queue_js/Solution.js
@@ -0,0 +1,71 @@
+class Node
+{
+    constructor(val)
+    {
+        this.val = val
+        this.next = null
+    }
+}
+
+class Queue
+{
+    constructor()
+    {
+        this.head = this.tail = null
+
+    }
+
+    push(val)
+    {
+        const node = new Node(val)
+        if (this.isEmpty())
+        {
+            this.head = node
+            this.tail = node
+        }
+        else
+        {
+            this.tail.next = node
+            this.tail = node
+        }
+    }
+
+    pop()
+    {
+        if (this.isEmpty())
+        {
+            console.log("List is empty !!")
+            return
+        }
+         this.head = this.head.next
+         if(this.head === null)
+         {
+            this.tail = null
+         }
+    }
+
+    front()
+    {
+        if(this.isEmpty())
+        {
+            console.log("LL is empty !!")
+            return -1
+        }
+        return this.head.val
+    }
+
+    isEmpty()
+    {
+        return this.head === null
+    }
+}
+
+
+let head = new Node(1)
+
+let node = new Node(2)
+
+head.next = node
+let Queue = head
+console.log(head.next.val)
+
