070fae4e605cfca11e998023f993e766346fea19
Satyam-R4j <satyamraj418@gmail.com>
2025-08-26 02:50:51 +0530

    Binary Tree Traversal methods

--- 12_BinarySearchTree_js/binarySearchTree.js
+++ 12_BinarySearchTree_js/binarySearchTree.js
@@ -51,6 +51,43 @@ class BinarySearchTree {
         }
         return null;
     }
+
+    inOrder(node = this.root) {
+        if (node === null) return null;
+        this.inOrder(node.left);
+        console.log(node.val);
+        this.inOrder(node.right);
+    }
+
+    preOrder(node = this.root) {
+        if (node === null) return null;
+        console.log(node.val);
+        this.preOrder(node.left);
+        this.preOrder(node.right);
+    }
+
+    postOrder(node = this.root) {
+        if (node === null) return null;
+        this.postOrder(node.left);
+        this.postOrder(node.right);
+        console.log(node.val);
+    }
+
+    // Level order traversal
+    levelOrder(node = this.root) {
+        if (node === null) return;
+        let qu = [node];
+        while (qu.length > 0) {
+            let curr = qu.shift();
+            console.log(curr.val);
+            if (curr.left !== null) {
+                qu.push(curr.left);
+            }
+            if (curr.right !== null) {
+                qu.push(curr.right);
+            }
+        }
+    }
 }
 
 let bst = new BinarySearchTree();
@@ -59,4 +96,5 @@ bst.insert(5);
 bst.insert(15);
 bst.insert(7);
 
+bst.levelOrder();
 console.log(JSON.stringify(bst, null, 2));
