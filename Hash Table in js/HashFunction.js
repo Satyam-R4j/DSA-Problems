f18d1623f722a611445532fb33a6bb1c82dbaa52
Satyam-R4j <satyamraj418@gmail.com>
2025-10-18 19:49:52 +0530

    Hash table

---
+++ Hash Table in js/HashFunction.js
@@ -0,0 +1,11 @@
+function hash(key, len) {
+    let total = 0;
+    for (let char of key) {
+        let value = char.charCodeAt(0) - 96;
+        total = (total + value) % len;
+    }
+    return total;
+}
+
+
+console.log(hash("Vikcy",9));
