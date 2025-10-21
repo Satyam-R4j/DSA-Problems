dd1740bc3a4ca73872400b21cdf17a649ac290e7
Satyam-R4j <satyamraj418@gmail.com>
2025-10-21 21:22:54 +0530

    WeightedGraph in js

---
+++ Dijkstra Algorithm in js/WeightedGraph.js
@@ -0,0 +1,17 @@
+class WeightedGraph{
+    constructor()
+    {
+        this.adjList = {}
+    }
+
+    addVertex(vertex)
+    {
+        if(!this.addVertex[vertex]) this.adjList[vertex] = []
+    }
+
+    addEdge(vertex1,vertex2,weight)
+    {
+        this.adjList[vertex1].push({node:vertex2,weight})
+        this.adjList[vertex2].push({node:vertex1,weight})
+    }
+}
